1a58aae6c8b7a33ceaa64d842076a01b22783102
Chad Ellison <chad.ellison0123@gmail.com>
2016-07-13 18:43:05 -0600

    Add directions

--- server.js
+++ server.js
@@ -11,20 +11,6 @@ app.get('/', function (req, res){
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-io.on('connection', function(socket) {
-  io.on('connection', function (channel, message) {
-  // socket.on('message', function (channel, message) {
-    io.sockets.emit("message", message)
-
-    console.log(channel + ':', message);
-    // });
-
-    });
-    socket.on('disconnect', function () {
-  });
-});
-
 http.listen(process.env.PORT || 8080, function(){
-  console.log('Your server is up and running on Port 8080. Good job!');
-  console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
+  console.log('Your server is up and running on Port 8080.');
 });
