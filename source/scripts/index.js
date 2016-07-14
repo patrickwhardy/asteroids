9cd491e2ac1d37576d97e8119fcf21157b3159f8
Chad Ellison <chad.ellison0123@gmail.com>
2016-07-13 19:26:52 -0600

    Webpack build

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -6,7 +6,6 @@ const Keyboard = require("./keyboard");
 var game = new Game(context, new Keyboard());
 
 requestAnimationFrame(function gameLoop() {
-  console.log('in rAF');
   context.clearRect(0, 0, canvas.width, canvas.height);
   game.gameText();
   game.update();
